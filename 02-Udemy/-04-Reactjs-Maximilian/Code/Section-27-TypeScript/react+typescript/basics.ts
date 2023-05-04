6298a86265acf874d75393512ecf52b5136274ff
Abo Ellil <mr.abuellil@gmail.com>
2023-05-04 09:54:59 +0300

    l

--- 02-Udemy/-04-Reactjs-Maximilian/Code/Section-27-TypeScript/react+typescript/basics.ts
+++ 02-Udemy/-04-Reactjs-Maximilian/Code/Section-27-TypeScript/react+typescript/basics.ts
@@ -1,29 +1,36 @@
-let age:number 
+let age: number;
 
-age=33
+age = 33;
 
-let userName:string = 'fdfd'
+let userName: string = "fdfd";
 
+let hobbies: string[];
 
-let hobbies:string[];
+hobbies = ["sports", "Cooking"];
 
- hobbies=['sports', 'Cooking']
-
-let person:{
-    name:string,
-    age:number
-}
+let person: {
+  name: string;
+  age: number;
+};
 
 person = {
-    name:'Mas',
-    age: 36
-}
-person = {
-    isWorking:true
-}
+  name: "Mas",
+  age: 36,
+};
+// person = {
+//     isWorking:true
+// }
 
+let people: {
+  name: string;
+  age: number;
+}[];
 
+//type inference
 
+let course = "type Script basicse";
+
+// course = 123456;
 
 
 
