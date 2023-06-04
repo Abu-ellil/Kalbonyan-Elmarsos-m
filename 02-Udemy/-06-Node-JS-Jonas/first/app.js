baf57c505d912deb6406a23abe124e035e09c635
Abo Ellil <mr.abuellil@gmail.com>
2023-06-05 02:07:43 +0300

    zz

---
+++ 02-Udemy/-06-Node-JS-Jonas/first/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+const textin = fs.readFileSync("./txt/input.txt", "utf-8");
+
+console.log(textin);
+
+const textOut = `this is what we know about the avocado: ${textin}.\nCreated on ${Date.now()}`
+
+
+
+fs.writeFileSync('./txt/output.txt', textOut )
+console.log('File written!');
